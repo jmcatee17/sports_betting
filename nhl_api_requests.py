5981c1937499aa52e630dd7cd83df87b3e71f748
jmcatee17 <jmcatee17@gmail.com>
2023-09-18 15:12:10 -0400

    Add skater_stats csv

--- nhl_api_requests.py
+++ nhl_api_requests.py
@@ -89,3 +89,158 @@ df_game = df[['game_id', 'season', 'type', 'date_time_GMT', 'away_team_id', 'hom
 
 df_game[['away_goals', 'home_goals']] = df_game[['away_goals', 'home_goals']].astype(int)
 
+# Define the NHL API endpoint for all NHL teams
+api_url = "https://statsapi.web.nhl.com/api/v1/teams"
+
+# Send a GET request to the NHL API to retrieve data about all NHL teams
+response = requests.get(api_url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()
+
+    # Extract the team IDs from the response
+    team_ids = [team["id"] for team in data["teams"]]
+
+    # Display the list of team IDs
+    print("Most Recent Team IDs:", team_ids)
+
+else:
+    print("Failed to retrieve data. Status code:", response.status_code)
+
+# Create an empty list to store skater data
+skater_data = []
+    
+# Iterate through each season
+for season in seasons:
+    # Iterate through each team ID
+    for team_id in team_ids:
+        # Define the NHL API endpoint for a specific team's game roster
+        api_url = f"https://statsapi.web.nhl.com/api/v1/teams/{team_id}?expand=team.roster"
+
+        # Send a GET request to the NHL API to retrieve the team's roster
+        response = requests.get(api_url)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+
+            # Extract the team's roster
+            roster = data["teams"][0]["roster"]["roster"]
+
+            # Iterate through each player on the roster
+            for player in roster:
+                player_id = player["person"]["id"]
+                player_name = player["person"]["fullName"]
+
+                # Define the NHL API endpoint for player game logs with the season parameter
+                player_game_url = f"https://statsapi.web.nhl.com/api/v1/people/{player_id}/stats?stats=gameLog&season={season}"
+
+                # Send a GET request to the NHL API to retrieve player game logs
+                response = requests.get(player_game_url)
+
+                # Check if the request was successful
+                if response.status_code == 200:
+                    player_game_data = response.json()
+
+
+                # Extract skater statistics for each game
+                for game_stats in player_game_data["stats"][0]["splits"]:
+                    game_data = {
+                        "team_id": team_id,
+                        "team_name": data["teams"][0]["name"],
+                        "opponent": game_stats["opponent"]["name"],
+                        "player_id": player_id,
+                        "player_name": player_name,
+                        "game_id": game_stats["game"]["gamePk"],
+                        "season": game_stats["season"],
+                        "goals": game_stats["stat"].get("goals"),
+                        "assists": game_stats["stat"].get("assists"),
+                        "shots": game_stats["stat"].get("shots"),
+                        "hits": game_stats["stat"].get("hits"),
+                        "powerPlayGoals": game_stats["stat"].get("powerPlayGoals"),
+                        "powerPlayAssists": game_stats["stat"].get("powerPlayAssists"),
+                        "time_on_ice": game_stats["stat"]["timeOnIce"],
+                        # Add more statistics as needed
+                    }
+                    skater_data.append(game_data)
+
+    else:
+        print(f"Failed to retrieve data for team ID {team_id}. Status code:", response.status_code)
+
+# Create a Pandas DataFrame from the skater data
+df_game_skater_stats = pd.DataFrame(skater_data)
+
+# Define the NHL API endpoints
+NHL_API_URL = "https://statsapi.web.nhl.com/api/v1/"
+SCHEDULE_ENDPOINT = "schedule"
+GAME_STATS_ENDPOINT = "game/{game_id}/boxscore"
+
+# Function to fetch game IDs for a specific season and game type
+def fetch_game_ids(season, game_type):
+    endpoint = f"{SCHEDULE_ENDPOINT}?season={season}&gameType={game_type}"
+    url = f"{NHL_API_URL}{endpoint}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    game_ids = []
+
+    for date in data["dates"]:
+        for game in date["games"]:
+            game_ids.append(game["gamePk"])
+
+    return game_ids
+
+# Function to fetch game teams stats for a specific game ID and team ID
+def fetch_game_teams_stats(game_id, team_id):
+    endpoint = f"{GAME_STATS_ENDPOINT}".format(game_id=game_id)
+    url = f"{NHL_API_URL}{endpoint}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    home_team_id = data["teams"]["home"]["team"]["id"]
+    away_team_id = data["teams"]["away"]["team"]["id"]
+    home_or_away = "home" if team_id == home_team_id else "away"
+    
+    # Calculate if the team won
+    home_goals = data["teams"]["home"]["teamStats"]["teamSkaterStats"]["goals"]
+    away_goals = data["teams"]["away"]["teamStats"]["teamSkaterStats"]["goals"]
+    won = home_goals > away_goals
+
+    game_teams_stats = {
+        "game_id": game_id,
+        "team_id": team_id,
+        "HoA": home_or_away,
+        "won": won,
+        # "settled_in": data["decisions"]["winner"] if won else data["decisions"]["loser"],
+        "head_coach": data["teams"]["home"]["coaches"][0]["person"]["fullName"],
+        "goals": home_goals if home_or_away == "home" else away_goals,
+        "shots": data["teams"][home_or_away]["teamStats"]["teamSkaterStats"]["shots"],
+        "hits": data["teams"][home_or_away]["teamStats"]["teamSkaterStats"]["hits"],
+        "pim": data["teams"][home_or_away]["teamStats"]["teamSkaterStats"]["pim"],
+    }
+
+    return game_teams_stats
+
+# Specify the seasons and game types you want to retrieve data for
+game_types = ["R", "P"]  # "R" for regular season, "P" for playoff
+
+# Fetch game teams stats data for the specified seasons and game types
+all_data = []
+
+for season in seasons:
+    for game_type in game_types:
+        game_ids = fetch_game_ids(season, game_type)
+        for game_id in game_ids:
+            # Fetch stats for both home and away teams
+            home_team_id = fetch_game_teams_stats(game_id, "home")
+            away_team_id = fetch_game_teams_stats(game_id, "away")
+            
+            # Append the stats for each team to the list
+            all_data.append(home_team_id)
+            all_data.append(away_team_id)
+
+# Create a DataFrame from the extracted data
+df_game_teams_stats = pd.DataFrame(all_data)
