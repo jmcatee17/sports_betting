f2c1e89fd5dfe746fc86ae11a49af79268190b82
jmcatee17 <jmcatee17@gmail.com>
2023-10-26 21:03:35 -0400

    Data Update

--- v2_nhl_api_calls/nhl_data_pipeline.py
+++ v2_nhl_api_calls/nhl_data_pipeline.py
@@ -12,7 +12,6 @@ os.chdir('/Users/jdmcatee/Desktop/sports_betting/data/maintained_tables')
 
 # Get each days data:
 start_date = datetime.today() - timedelta(days=1)
-start_date = start_date.strftime('%Y-%m-%d')
 end_date = datetime.today().strftime('%Y-%m-%d')
 
 # Get updated data for start date and end date
@@ -33,4 +32,3 @@ df_updated_game.to_csv('fct_maintained_game.csv', index = False)
 df_updated_game_teams_stats.to_csv('fct_maintained_game_teams_stats.csv', index = False)
 df_updated_game_skater_stats.to_csv('fct_maintained_game_skater_stats.csv',index = False)
 df_updated_game_goalie_stats.to_csv('fct_maintained_game_goalie_stats.csv', index = False)
-
