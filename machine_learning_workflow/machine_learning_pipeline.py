15fa6b93aa8a876bf4ba64fdedee99379a25ed6c
jmcatee17 <jmcatee17@gmail.com>
2023-10-29 22:08:17 -0400

    Update ML Pipeline

--- machine_learning_workflow/machine_learning_pipeline.py
+++ machine_learning_workflow/machine_learning_pipeline.py
@@ -1,7 +1,19 @@
 # Import necessary packages
+from functools import partial
+import pickle
 import pandas as pd
 import numpy as np
 import os
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import SGDRegressor
+from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.model_selection import train_test_split, cross_val_score, RepeatedKFold, GridSearchCV, StratifiedKFold
+from xgboost import XGBRegressor
+
+# Skopt functions
+from skopt import BayesSearchCV
+from skopt.callbacks import DeadlineStopper, DeltaYStopper
+from skopt.space import Real, Categorical, Integer
 
 # Change Working directory
 os.chdir('/Users/jdmcatee/Desktop/sports_betting/data/maintained_tables')
@@ -23,6 +35,43 @@ for t in [df_game, df_game_skater_stats, df_game_teams_stats]:
         t[c] = t[c].astype(str)
     counter +=1
 
+# Start Analysis from 20102011 season forwards (due to missing faceoffpercentage data before)
+start_season = '20102011'
+start_game = '2010020001'
+
+# Filter DataFrames:
+df_game = df_game[(df_game['season'] >= start_season) & (df_game['game_state'] != 'FUT')]
+df_game_skater_stats = df_game_skater_stats[df_game_skater_stats['game_id'] >= start_game]
+df_game_teams_stats = df_game_teams_stats[df_game_teams_stats['game_id'] >= start_game]
+
+def time_to_seconds(time_str):
+    time_str = str(time_str)
+    time_parts = time_str.split(':')
+    if len(time_parts) == 0:
+        return time_str
+    if len(time_parts) == 2:
+        hours, minutes = map(int, time_parts)
+        return hours * 3600 + minutes * 60
+    elif len(time_parts) == 3:
+        hours, minutes, seconds = map(int, time_parts)
+        return hours * 3600 + minutes * 60 + seconds
+
+# # Apply the function to the 'Time' column and create a new 'Seconds' column
+df_game_skater_stats['time_on_ice'] = df_game_skater_stats['time_on_ice'].apply(lambda x: time_to_seconds(x))
+
+for c in ['home_goals', 'away_goals']:
+    df_game[c] = df_game[c].astype(int)
+
+for c in ['time_on_ice', 'assists', 'goals', 'shots' ,'hits', 'power_play_goals', 'power_play_assists']:
+    df_game_skater_stats[c] = df_game_skater_stats[c].fillna(0).astype(int)
+
+df_game_teams_stats.loc[df_game_teams_stats['won'] == 'True', 'won'] = 1
+df_game_teams_stats.loc[df_game_teams_stats['won'] == 'False', 'won'] = 0
+for c in ['goals', 'shots', 'hits', 'won', 'pim', 'blocks', 'powerPlayOpportunities', 'powerPlayGoals']:
+    df_game_teams_stats[c] = df_game_teams_stats[c].fillna(0).astype(int)
+
+df_game_teams_stats['faceoffWinningPctg'] = df_game_teams_stats['faceoffWinningPctg'].astype(float)
+
 def top_scorers(df_game_skater_stats = df_game_skater_stats):
     # Find top 20 goal scorers by season:
 
@@ -35,12 +84,16 @@ def top_scorers(df_game_skater_stats = df_game_skater_stats):
     # df_result_grouped_names = pd.merge(df_result_grouped, df_player_info, how = 'left', on = ['player_id'])
     # df_top_scorers = df_result_grouped_names.groupby(['season'])['player_id'].apply(list).reset_index(name='top_scorers_players_ids')
     df_top_scorers = df_result_grouped.groupby(['season'])['player_id'].apply(list).reset_index(name='top_scorers_players_ids')
-    df_top_scorers['top_scorers_players_ids'] = df_top_scorers['top_scorers_players_ids'].apply(lambda x: x[:20])
+    df_top_scorers['top_scorers_players_ids'] = df_top_scorers['top_scorers_players_ids'].apply(lambda x: list(x[:20]))
 
     return df_top_scorers
 
 def count_overlaps(list1, list2):
-    return len(set(list1) & set(list2))
+    try:
+        return len(set(list1) & set(list2))
+    except TypeError:
+            # Handle the case when list1 or list2 is not iterable
+            return 0
 
 def corsi_statistic(shots_for, shots_against, blocks_for, blocks_against):
     corsi_for = shots_for + blocks_for
@@ -144,7 +197,7 @@ def create_rolling_stats(df_game_teams_stats = df_game_teams_stats, df_prep = da
     df_merged = df_merged[df_merged.type == 'R']
     # Create window functions for season averages
 
-    df_rolling = df_merged.sort_values(by = ['season', 'team_id', 'game_number'], ascending = True)
+    df_rolling = df_merged.sort_values(by = ['season', 'team_id', 'game_number'], ascending = True).reset_index(drop=True)
     rolling_dictionary = {
                         "goals" : f"{game_window}_game_rolling_goals",
                         "shots" : f"{game_window}_game_rolling_shots",
@@ -168,7 +221,7 @@ def create_rolling_stats(df_game_teams_stats = df_game_teams_stats, df_prep = da
                         "blocks_against" : f"{game_window}_game_rolling_blocked_against",
                       }
     for key, value in rolling_dictionary.items():
-        df_rolling[value] = df_rolling.groupby(['season', 'team_id'])[key].apply(lambda x: x.rolling(window = game_window, min_periods=1).mean().shift(1))
+        df_rolling[value] = df_rolling.groupby(['season', 'team_id'])[key].apply(lambda x: x.rolling(window = game_window, min_periods=1).mean().shift(1)).reset_index(drop=True)
     
     # Drop Index Column
     df_rolling = df_rolling.drop(columns = ['index']).reset_index(drop = True)
@@ -228,10 +281,9 @@ def add_top_scorers_and_goalie_counts(df_game_skater_stats = df_game_skater_stat
     df_season_merge = pd.merge(df_stg1, df_season_map, on = ['game_id'], how = 'left')
     df_top_players_merge = pd.merge(df_season_merge, df_top_scorers, on = 'season', how = 'left')
 
-    # Convert game_id, team_id, opponent_id, season to strings
-    for c in ['game_id', 'team_id']:
-        df_top_players_merge[c] = df_top_players_merge[c].astype(str)
-    
+    df_top_players_merge['top_scorers_players_ids'] = df_top_players_merge['top_scorers_players_ids'].tolist()
+    df_top_players_merge['game_player_ids'] = df_top_players_merge['game_player_ids'].tolist()
+
     # Find unique overlaps between players and top scorers in each game (ie how many top scorers playing for each team)
     df_top_players_merge['count_top_players'] = df_top_players_merge.apply(lambda x: count_overlaps(x['top_scorers_players_ids'], x['game_player_ids']), axis = 1)
     df_top_players_merge = df_top_players_merge[['game_id', 'team_id', 'count_top_players']]
@@ -246,4 +298,141 @@ def add_top_scorers_and_goalie_counts(df_game_skater_stats = df_game_skater_stat
     
     return df_output
 
-print(add_top_scorers_and_goalie_counts().info())
+# Create Train - Test Split
+df_i = add_top_scorers_and_goalie_counts()
+df_i = df_i.dropna()
+drop_list = ['game_id', 'team_id', 'season', 'date_time_GMT', 'venue', 'opponent_id', 'type', 'settled_in', 'won', 'hits', 'shots', 'pim', 'powerPlayOpportunities', 'powerPlayGoals', 'blocks']
+append_list = ['home_against', 'index_against', 'goals_against']
+for i in drop_list:
+    if i != 'game_id':
+        append_list.append(i + '_against')
+drop_list = drop_list + append_list
+
+df_i = df_i.drop(columns = drop_list)
+
+# Set feature and target variables
+X = df_i.drop(columns = 'goals')
+y = df_i[['goals']]
+
+# Apply train test split to the data, random state of 42 for reproducibility
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(X.info())
+
+# Baseline Random Forest Regressor
+rf = RandomForestRegressor(random_state=42)
+rf.fit(X_train, y_train)
+
+# Predict and compare:
+rf_preds = rf.predict(X_test)
+rf_preds = pd.DataFrame(rf_preds, columns = ['prediction'])
+
+# Compute summary statistics:
+mse = mean_squared_error(y_test, rf_preds)
+rmse = mse**.5
+print("Random Forest RMSE: ", rmse)
+pickle.dump(rf, open('random_forest.sav', 'wb'))
+
+# create an xgboost regression model
+xgb = SGDRegressor(n_estimator = 800, eta = .01, max_depth = 10)
+
+# define model evaluation method
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+# evaluate model
+scores = cross_val_score(xgb, X, y, scoring='neg_mean_squared_error', cv=cv, n_jobs=-1)
+#force scores to be positive
+scores = abs(scores)** (1/2)
+
+xgb.fit(X_train, y_train)
+print('XGBoost Mean RMSE: %.3f (%.3f)' % (scores.mean(), scores.std()))
+pickle.dump(xgb, open('xgboost.sav', 'wb'))
+
+# Reporting util for different optimizers
+def report_perf(optimizer, X, y, title="model", callbacks=None):
+    """
+    A wrapper for measuring time and performances of different optmizers
+    
+    optimizer = a sklearn or a skopt optimizer
+    X = the training set 
+    y = our target
+    title = a string label for the experiment
+    """
+    start = time()
+    
+    if callbacks is not None:
+        optimizer.fit(X, y, callback=callbacks)
+    else:
+        optimizer.fit(X, y)
+        
+    d=pd.DataFrame(optimizer.cv_results_)
+    best_score = optimizer.best_score_
+    best_score_std = d.iloc[optimizer.best_index_].std_test_score
+    best_params = optimizer.best_params_
+    
+    print((title + " took %.2f seconds,  candidates checked: %d, best CV score: %.3f "
+           + u"\u00B1"+" %.3f") % (time() - start, 
+                                   len(optimizer.cv_results_['params']),
+                                   best_score,
+                                   best_score_std))    
+    print('Best parameters:')
+    print(best_params)
+    return best_params
+
+# Setting the scoring function
+scoring = make_scorer(partial(mean_squared_error, squared = False), 
+                      greater_is_better = False)
+
+# Setting the validation strategy
+skf = StratifiedKFold(n_splits = 7,
+                      shuffle = True, 
+                      random_state = 42)
+
+cv_strategy = list(skf.split(X_train, y_train))
+
+# Setting the basic regressor
+xgb_bayes = XGBRegressor(random_state = 42, booster = 'gbtree', objective = 'reg:squarederror', tree_method = 'hist')
+
+# Setting the search space
+search_spaces = {'learning_rate': Real(0.01, 0.1, 'uniform'),
+                 'max_depth': Integer(2, 8),
+                 'subsample': Real(0.1, 1.0, 'uniform'),
+                 'colsample_bytree': Real(0.1, 1.0, 'uniform'), # subsample ratio of columns by tree
+                 #'reg_lambda': Real(1e-9, 100., 'uniform'), # L2 regularization
+                 #'reg_alpha': Real(1e-9, 100., 'uniform'), # L1 regularization
+                 'n_estimators': Integer(350, 900)
+   }
+
+
+# Wrapping everything up into the Bayesian optimizer
+opt = BayesSearchCV(estimator= xgb_bayes,                                    
+                    search_spaces = search_spaces,                      
+                    scoring = scoring,                                  
+                    cv = cv_strategy,                                           
+                    n_iter = 120,                                       # max number of trials
+                    n_points = 1,                                       # number of hyperparameter sets evaluated at the same time
+                    n_jobs = 1,                                       # if not iid it optimizes on the cv score
+                    return_train_score = False,                         
+                    refit = False,                                      
+                    optimizer_kwargs = {'base_estimator': 'GP'},        # optmizer parameters: we use Gaussian Process (GP)
+                    random_state = 42)                                   # random state for replicability
+
+# Running the optimizer
+overdone_control = DeltaYStopper(delta=0.0001)                    # We stop if the gain of the optimization becomes too small
+time_limit_control = DeadlineStopper(total_time=60*60*4)          # We impose a time limit (4 hours)
+
+best_params = report_perf(opt, X_train, y_train,'XGBoost_regression', 
+                          callbacks = [overdone_control, time_limit_control])
+
+# Transferring the best parameters to our basic regressor
+xgb_bayes = XGBRegressor(random_state=0, booster='gbtree', objective='reg:squarederror', tree_method='hist', **best_params)
+xgb_bayes.fit(X_train, y_train)
+
+xgb_bayes_preds = xgb_bayes.predict(X_test)
+xgb_bayes_preds = pd.DataFrame(xgb_bayes_preds, columns = ['prediction'])
+
+# Compute summary statistics:
+mse = mean_squared_error(y_test, xgb_bayes_preds)
+rmse = mse**.5
+print("XGBoost Hyperparameter Optimization RMSE: ", rmse)
+pickle.dump(xgb_bayes, open('xgboost_bayes.sav', 'wb'))
